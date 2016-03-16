b29ab5041d6fee026d415afe58fdfa52783332d8
itzseven <iSeven@macbook-pro-de-romain-2.local>
2016-03-16 12:57:17 +0100

    interests : new component to handle interest points filter : new component to apply filters on images

---
+++ src/core/interests.c
@@ -0,0 +1,19 @@
+//
+//  interests.c
+//  imagic
+//
+//  Created by itzseven on 24/02/2016.
+//  Copyright © 2016 itzseven. All rights reserved.
+//
+
+#include "interests.h"
+
+pt2darray_t *itrtharris(gray8i_t *src, float lambda, uint32_t count)
+{
+    
+}
+
+pt2darray_t *itrtgrad(gray8i_t *src, uint32_t count)
+{
+    
+}
