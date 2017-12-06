ddc7528534979337c69904103f53dbb18fe76230
itzseven <azik_95@hotmail.fr>
2017-12-06 14:31:09 +0100

    fix(project) : add unit tests + testing library and remove unused files

---
+++ Libraries/cunit/include/cunit.h
@@ -0,0 +1,17 @@
+//
+//  cunit.h
+//  cunit
+//
+//  Created by itzseven on 28/11/2017.
+//  Copyright © 2017 itzseven. All rights reserved.
+//
+
+#ifndef cunit_h
+#define cunit_h
+
+#include "ctcase.h"
+#include "ctperf.h"
+#include "ctsuite.h"
+#include "cassert.h"
+
+#endif /* cunit_h */
---
+++ UnitTests/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  UnitTests
+//
+//  Created by Romain DUBREUCQ on 06/12/2017.
+//  Copyright © 2017 Romain Dubreucq. All rights reserved.
+//
+
+#include <stdio.h>
+#include "cunit.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
