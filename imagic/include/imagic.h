83d8cded3d87f022c9554e88a1e43848d6f73e53
itzseven <azik_95@hotmail.fr>
2017-12-06 14:36:00 +0100

    fix(headers) : add main header

---
+++ imagic/include/imagic.h
@@ -0,0 +1,13 @@
+//
+//  imagic.h
+//  imagic
+//
+//  Created by itzseven on 06/12/2017.
+//  Copyright © 2017 itzseven. All rights reserved.
+//
+
+#ifndef imagic_h
+#define imagic_h
+
+
+#endif /* imagic_h */
