a56b3c6cd98c624e3cd0f4001a47324842abbeb0
Romain <itzseven@macbook-pro-de-romain.local>
2018-01-03 18:02:29 +0100

    refactor(image) : - replace rgb8i and gray8i with generic datatype img_t based on matrix component - add macros to retrieve a pixel, a component -

--- imagic/image.c
+++ imagic/image.c
@@ -7,72 +7,149 @@
 //
 
 #include "image.h"
-#include "matrix.h"
+#include "_image.h"
 #include <limits.h>
 
 #pragma
 #pragma mark - Allocation copy deallocation
 
-rgb8i_t *rgb8i(unsigned int width, unsigned int height) {
-    return mat(width, height, 1, sizeof(rgb8i_t));
+img_t *img(unsigned int width, unsigned int height, unsigned int depth, size_t dsize) {
+    return mat(width, height, depth, dsize);
 }
 
-rgb8i_t *rgb8icpy(const rgb8i_t *src) {
-    return (rgb8i_t *)matcpy((mat_t *)src);
+img_t *imgcpy(const img_t *img) {
+    return matcpy((const mat_t *)img);
 }
 
-void rgb8idcpy(rgb8i_t *dst, const rgb8 *data) {
-    return matdcpy((mat_t *)dst, data);
+void _img8dcpy(const img_t *dst, const void *data) {
+    matdcpy(dst, (const uint8_t *)data);
 }
 
-void rgb8ifree(rgb8i_t *img) {
-    matfree((mat_t *)img);
+void _img16dcpy(const img_t *dst, const void *data) {
+    matdcpy(dst, (const uint16_t *)data);
 }
 
-gray8i_t *gray8i(unsigned int width, unsigned int height) {
-    return mat(width, height, 1, sizeof(uint8_t));
+void _img32dcpy(const img_t *dst, const void *data) {
+    matdcpy(dst, (const uint32_t *)data);
 }
 
-gray8i_t *gray8icpy(const gray8i_t *src) {
-    return (gray8i_t *)matcpy((mat_t *)src);
+void _img64dcpy(const img_t *dst, const void *data) {
+    matdcpy(dst, (const uint64_t *)data);
 }
 
-void gray8idcpy(gray8i_t *dst, const uint8_t *data) {
-    return matdcpy((mat_t *)dst, data);
+void imgdcpy(const img_t *dst, const void *data) {
+    void (*_imgdcpy[4]) (const img_t *dst, const void *data) = {_img8dcpy, _img16dcpy, _img32dcpy, _img64dcpy};
+    _imgdcpy[dst->dsize >> 1](dst, data);
 }
 
-void gray8ifree(gray8i_t *img) {
-    matfree((mat_t *)img);
+void imgfree(img_t *img) {
+    matfree(img);
 }
 
 #pragma
 #pragma mark - Comparison
 
-unsigned int rgb8icmp(const rgb8i_t *img1, const rgb8i_t *img2) {
-    if ((img1->width != img2->width) || (img1->height != img2->height)) {
-        return UINT_MAX;
-    }
-    
-    unsigned int diff = 0, i = 0, len = img1->width * img1->height;
+unsigned int _img8cmp(const img_t *img1, const img_t *img2) {
+    unsigned int diff = 0, i = 0, len = img1->width * img1->height * img1->depth;
     
     for (i = len; i-- ; ) {
-        register rgb8 img1val = img1->data[i], img2val = img2->data[i];
-        diff += (img1val.r != img2val.r) || (img1val.g != img2val.g) || (img1val.b != img2val.b);
+        diff += ELT_DATA(img1, uint8_t, i) != ELT_DATA(img2, uint8_t, i);
     }
     
     return diff;
 }
 
-unsigned int gray8icmp(const gray8i_t *img1, const gray8i_t *img2) {
-    if ((img1->width != img2->width) || (img1->height != img2->height)) {
-        return UINT_MAX;
-    }
-    
-    unsigned int diff = 0, i = 0, len = img1->width * img1->height;
+unsigned int _img16cmp(const img_t *img1, const img_t *img2) {
+    unsigned int diff = 0, i = 0, len = img1->width * img1->height * img1->depth;
     
     for (i = len; i-- ; ) {
-        diff += img1->data[i] != img2->data[i];
+        diff += ELT_DATA(img1, uint16_t, i) != ELT_DATA(img2, uint16_t, i);
     }
     
     return diff;
 }
+
+unsigned int _img32cmp(const img_t *img1, const img_t *img2) {
+    unsigned int diff = 0, i = 0, len = img1->width * img1->height * img1->depth;
+    
+    for (i = len; i-- ; ) {
+        diff += ELT_DATA(img1, uint32_t, i) != ELT_DATA(img2, uint32_t, i);
+    }
+    
+    return diff;
+}
+
+unsigned int _img64cmp(const img_t *img1, const img_t *img2) {
+    unsigned int diff = 0, i = 0, len = img1->width * img1->height * img1->depth;
+    
+    for (i = len; i-- ; ) {
+        diff += ELT_DATA(img1, uint64_t, i) != ELT_DATA(img2, uint64_t, i);
+    }
+    
+    return diff;
+}
+
+unsigned int imgcmp(const img_t *img1, const img_t *img2) {
+    if ((img1->width != img2->width) || (img1->height != img2->height) || (img1->depth != img2->depth) || (img1->dsize != img2->dsize)) {
+        return UINT_MAX;
+    }
+    
+    unsigned int (*_imgcmp[4]) (const img_t *img1, const img_t *img2) = {_img8cmp, _img16cmp, _img32cmp, _img64cmp};
+    return _imgcmp[img1->dsize >> 1](img1, img2);
+}
+
+#pragma mark - Private
+
+unsigned int img8max(const img_t *img) {
+    unsigned int i = 0, len = img->width * img->height * img->depth;
+    uint8_t max = 0;
+    
+    for (i = len; i--; ) {
+        uint8_t p = ELT_DATA(img, uint8_t, i);
+        max = ((p > max) * p) + ((p <= max) * max);
+    }
+    
+    return (unsigned int)max;
+}
+
+unsigned int img16max(const img_t *img) {
+    unsigned int i = 0, len = img->width * img->height * img->depth;
+    uint16_t max = 0;
+    
+    for (i = len; i--; ) {
+        uint16_t p = ELT_DATA(img, uint16_t, i);
+        max = ((p > max) * p) + ((p <= max) * max);
+    }
+    
+    return (unsigned int)max;
+}
+
+unsigned int img32max(const img_t *img) {
+    unsigned int i = 0, len = img->width * img->height * img->depth;
+    uint32_t max = 0;
+    
+    for (i = len; i--; ) {
+        uint32_t p = ELT_DATA(img, uint32_t, i);
+        max = ((p > max) * p) + ((p <= max) * max);
+    }
+    
+    return (unsigned int)max;
+}
+
+unsigned int img64max(const img_t *img) {
+    unsigned int i = 0, len = img->width * img->height * img->depth;
+    uint64_t max = 0;
+    
+    for (i = len; i--; ) {
+        uint64_t p = ELT_DATA(img, uint64_t, i);
+        max = ((p > max) * p) + ((p <= max) * max);
+    }
+    
+    return (unsigned int)max;
+}
+
+unsigned int imgmax(const img_t *img) {
+    unsigned int (*_imgmax[4]) (const img_t *img) = {img8max, img16max, img32max, img64max};
+    return _imgmax[img->dsize >> 1](img);
+}
+
---
+++ imagic/include/private/_image.h
@@ -0,0 +1,16 @@
+//
+//  _image.h
+//  imagic
+//
+//  Created by itzseven on 03/01/2018.
+//  Copyright © 2018 itzseven. All rights reserved.
+//
+
+#ifndef _image_h
+#define _image_h
+
+#include "image.h"
+
+unsigned int imgmax(const img_t *img);
+
+#endif /* _image_h */
