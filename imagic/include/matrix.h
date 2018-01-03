b42c19f96f6e7f6be61c8c8447f588de10eee592
Romain <itzseven@macbook-pro-de-romain.local>
2018-01-03 17:56:44 +0100

    refactor(matrix) : - introduces depth - add new macro to get a element - optimizes code

--- imagic/include/matrix.h
+++ imagic/include/matrix.h
@@ -11,14 +11,15 @@
 
 #include <stdlib.h>
 
-#define ELT_IDX(mat, x, y) (((y) * mat->width) + (x))
-#define ELT(mat, datatype, x, y) ((datatype *)mat->data)[ELT_IDX(mat, x, y)]
+#define ELT_DATA(mat, dtype, index) ((dtype *)mat->data)[index]
+#define ELT_IDX(mat, x, y, d) (((y) * mat->width) + (x) + d)
+#define ELT(mat, dtype, x, y, d) ELT_DATA(mat, dtype, ELT_IDX(mat, x, y, d))
 
 #pragma
 #pragma mark - Matrix datatype
 
 typedef struct mat_t {
-    unsigned int width, height;
+    unsigned int width, height, depth;
     size_t dsize;
     void *data;
 }mat_t;
@@ -26,9 +27,9 @@ typedef struct mat_t {
 #pragma
 #pragma mark - Allocation copy deallocation
 
-void *mat(unsigned int width, unsigned int height, size_t dsize);
+void *mat(unsigned int width, unsigned int height, unsigned int depth, size_t dsize);
 mat_t *matcpy(const mat_t *mat);
-void matdcpy(mat_t *mat, const void *data);
+void matdcpy(const mat_t *mat, const void *data);
 void matfree(mat_t *mat);
 
 #endif /* matrix_h */
