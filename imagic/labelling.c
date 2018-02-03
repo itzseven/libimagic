065b0e17c37d221ddc49bd353832fceb5785e2b7
itzseven <azik_95@hotmail.fr>
2018-02-03 18:09:31 +0100

    feat(labelling) : optimize label function

--- imagic/labelling.c
+++ imagic/labelling.c
@@ -143,33 +143,33 @@ labels_t * label2(const img_t *src)
     unsigned int width = src->width, height = src->height;
     
     uint32_t MAX_LABELS = ceilf(((float)(src->width - 2) / 2)) * (src->height - 2);
+    uint32_t i = 0, k = 0, l = 1, m = 1, eB = 0, eA = 0, eC = 0, attA = 0, attB = 0, attC = 0, jm1_i = 0, j_im1 = 0, j_i = 0;
     
-    uint32_t i = 0, k = 0, l = 1, m = 1, eB = 0, eA = 0, eC = 0, attA = 0, attB = 0, attC = 0;
     unsigned int *T = (unsigned int *)calloc(MAX_LABELS, sizeof(unsigned int));
     
     unsigned int TeB = 0;
     
-    uint8_t attA_eq_attC = 0, attB_eq_attC = 0, eA_eq_eB = 0;
-    
-    uint32_t jm1_i = 0, j_im1 = 0, j_i = 0;
+    bool attA_eq_attC = 0, attB_eq_attC = 0, eA_eq_eB = 0;
     
     for (k = MAX_LABELS; k--; ) {
         T[k] = k;
     }
     
     uint32_t start = width + 1, end = ((width - 1) * height) - 1;
+    uint8_t *imgdata = (uint8_t *)src->data;
+    uint32_t *labels = (uint32_t *)newLabels->data;
     
     for (i = start; i < end; i++) {
         jm1_i = i - 1;
         j_im1 = i - width;
         j_i = i;
         
-        attA = ELT_DATA(src, uint8_t, jm1_i);
-        attB = ELT_DATA(src, uint8_t, j_im1);
-        attC = ELT_DATA(src, uint8_t, j_i);
+        attA = imgdata[jm1_i];
+        attB = imgdata[j_im1];
+        attC = imgdata[j_i];
         
-        eA = ELT_DATA(newLabels, uint32_t, jm1_i);
-        eB = ELT_DATA(newLabels, uint32_t, j_im1);
+        eA = labels[jm1_i];
+        eB = labels[j_im1];
         
         TeB = T[eB];
         
@@ -179,22 +179,22 @@ labels_t * label2(const img_t *src)
         
         if (attC == 0) {
             if (attA_eq_attC && !attB_eq_attC)
-                ELT_DATA(newLabels, uint32_t, j_i) = eA;
+                labels[j_i] = eA;
             
             else if (attB_eq_attC && !attA_eq_attC)
-                ELT_DATA(newLabels, uint32_t, j_i) = TeB;
+                labels[j_i] = TeB;
             
             else if (!attB_eq_attC && !attA_eq_attC)
-                ELT_DATA(newLabels, uint32_t, j_i) = ++newLabels->count;
+                labels[j_i] = ++newLabels->count;
             
             else if (attB_eq_attC && attA_eq_attC && eA_eq_eB)
-                ELT_DATA(newLabels, uint32_t, j_i) = eA;
+                labels[j_i] = eA;
             
             else if (attB_eq_attC && attA_eq_attC && !eA_eq_eB)
             {
                 uint8_t TeB_leq_eA = TeB < eA;
-                ELT_DATA(newLabels, uint32_t, j_i) = (TeB_leq_eA) ? TeB : eA;
-                eC = ELT_DATA(newLabels, uint32_t, j_i);
+                labels[j_i] = (TeB_leq_eA) ? TeB : eA;
+                eC = labels[j_i];
                 T[eC] = eC;
                 T[eA] = eC;
                 T[(!TeB_leq_eA) ? TeB : eA] = eC;
@@ -215,8 +215,8 @@ labels_t * label2(const img_t *src)
     uint32_t lab = 0;
     
     for (i = start; i < end; i++) {
-        lab = ELT_DATA(newLabels, uint32_t, i);
-        lab = ELT_DATA(newLabels, uint32_t, i) = T[lab];
+        lab = labels[i];
+        lab = labels[i] = T[lab];
         
         bool newMaxLabel = lab > maxLabel;
         maxLabel = (newMaxLabel * lab) + (!newMaxLabel * maxLabel);
